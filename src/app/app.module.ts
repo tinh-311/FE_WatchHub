9f5c3ffd8694e4d23e7b54404bdc52d7f19a9b5e
timmo <timmo.n@itcgroup.io>
2023-05-13 20:27:21 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,32 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from './header/header.component';
+
+import { ToolbarModule } from 'primeng/toolbar';
+import { LoginComponent } from './login/login.component';
+import { HeaderLoginComponent } from './login/header-login/header-login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    HeaderLoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+    ToolbarModule,
+    DateInputsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/header-login/header-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-login',
+  templateUrl: './header-login.component.html',
+  styleUrls: ['./header-login.component.scss']
+})
+export class HeaderLoginComponent {
+
+}
