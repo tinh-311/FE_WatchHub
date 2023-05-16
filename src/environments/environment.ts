7eea5a1a7be78189b773249615609e5b3de490db
timmo <timmo.n@itcgroup.io>
2023-05-16 23:01:25 +0700

    Feature - register

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,10 @@
-export const environment = {
+export const firebaseConfig = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyBEfkmx1fl2iy2EjYwNtW9eQcZR0oQLhuw",
+  apiKey: "AIzaSyBEfkmx1fl2iy2EjYwNtW9eQcZR0oQLhuw",
   authDomain: "watchhub-386708.firebaseapp.com",
   projectId: "watchhub-386708",
   storageBucket: "watchhub-386708.appspot.com",
   messagingSenderId: "611260875244",
   appId: "1:611260875244:web:b91532a10fdda0a31cc0c9",
   measurementId: "G-V1V8N2RYNV"
-  }
 };
