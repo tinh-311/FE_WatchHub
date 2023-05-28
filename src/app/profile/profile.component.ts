c9711c9174a8cd5ef2eda34cc812a9d14b8b5d75
timmo <timmo.n@itcgroup.io>
2023-05-28 15:23:53 +0700

    Upload avatar

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AddressVNService } from 'src/service/address-vn.service';
 import { AddressService } from 'src/service/address.service';
-import { ToastType } from 'src/service/constant/toast.constant';
+import { ToasSumary, ToastType } from 'src/service/constant/toast.constant';
 import { GeolocationService } from 'src/service/geolocation.service';
 import { ToastService } from 'src/service/toast.service';
 import {
+  UUID_DEFAUL_AVATAR,
   formatAddress,
   normalizeName,
   parseAddressData,
@@ -14,6 +15,15 @@ import { FilterService } from 'primeng/api';
 import { LoadingService } from 'src/service/loading.service';
 import { Geolocation } from '@capacitor/geolocation';
 import jwt_decode from 'jwt-decode';
+import * as LR from '@uploadcare/blocks';
+import type { UploadcareFile } from '@uploadcare/upload-client';
+import { UserService } from 'src/service/user.service';
+
+LR.registerBlocks(LR);
+
+type UploadcareBlocksFile = UploadcareFile & {
+  cdnUrlModifiers: string | null;
+};
 
 @Component({
   selector: 'app-profile',
@@ -36,6 +46,8 @@ export class ProfileComponent implements OnInit {
 
   currentUser: any;
   address: any;
+  uploadedUuid: string = '';
+  readonly UUID_DEFAUL_AVATAR = UUID_DEFAUL_AVATAR;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -44,13 +56,14 @@ export class ProfileComponent implements OnInit {
     private geolocationService: GeolocationService,
     private addressByGeoService: AddressService,
     private filterService: FilterService,
-    private loadingService: LoadingService
-  ) {}
-
-  ngOnInit() {
+    private loadingService: LoadingService,
+    private userService: UserService
+  ) {
     const token = localStorage.getItem('token');
     if (token) {
       this.currentUser = jwt_decode(token);
+      console.log('🏍️ ~ this.currentUser: ', this.currentUser);
+      this.updateLRImgElementWithUuid(this.currentUser?.avatar);
       this.infoForm = this.formBuilder.group({
         fullName: [this.currentUser?.fullname, [Validators.required]],
         email: [
@@ -65,12 +78,53 @@ export class ProfileComponent implements OnInit {
         totalOrder: [0, [Validators.required]],
       });
     }
+  }
+
+  ngOnInit() {
+    window.addEventListener('LR_DATA_OUTPUT', (e: any) => {
+      this.loadingService.showLoading();
+      this.uploadedUuid = e.detail?.data[0]?.uuid;
+      this.currentUser.avatar = this.uploadedUuid;
+      let userUpdate = {
+        id: this.currentUser?.id,
+        username: this.currentUser?.username || '',
+        fullname: this.currentUser?.fullname || '',
+        phone: this.currentUser?.phone || '',
+        address: this.currentUser?.address || '',
+        avatar: this.currentUser?.avatar || '',
+      };
+
+      this.userService.updateUser({ ...userUpdate }).subscribe(
+        (res) => {
+          localStorage.setItem('token', res?.token);
+          this.loadingService.hideLoading();
+          this.toastService.showMessage(
+            ToasSumary.Success,
+            res?.message,
+            ToastType.Success
+          );
+        },
+        (err) => {
+          this.toastService.showMessage(
+            ToasSumary.Error,
+            err?.error?.message,
+            ToastType.Error
+          );
+          this.loadingService.hideLoading();
+        }
+      );
+      this.uploadedUuid = this.uploadedUuid
+        ? this.uploadedUuid
+        : UUID_DEFAUL_AVATAR;
+      this.updateLRImgElementWithUuid(this.uploadedUuid);
+    });
 
     this.loadingService.showLoading();
     this.addressService.getProvinces().subscribe(
       (res: any) => {
         this.provinces = res?.data?.data;
         this.loadingService.hideLoading();
+        this.updateLRImgElementWithUuid(this.currentUser?.avatar);
       },
       (err) => {
         this.loadingService.hideLoading();
@@ -79,8 +133,31 @@ export class ProfileComponent implements OnInit {
           err?.error?.message,
           ToastType.Error
         );
+        this.updateLRImgElementWithUuid(this.currentUser?.avatar);
       }
     );
+    this.updateLRImgElementWithUuid(this.currentUser?.avatar);
+  }
+
+  updateLRImgElementWithUuid(uploadedUuid: string) {
+    const lrImgElement = document.getElementById(
+      'user-avatar-profile'
+    ) as HTMLElement;
+    if (lrImgElement) {
+      lrImgElement.setAttribute('uuid', uploadedUuid);
+    }
+
+    const lrImgElement2 = document.getElementById('user-avatar') as HTMLElement;
+    if (lrImgElement) {
+      lrImgElement2.setAttribute('uuid', uploadedUuid);
+    }
+  }
+
+  files: UploadcareBlocksFile[] = [];
+
+  handleUploaderEvent(e: Event) {
+    const { data: files } = (e as CustomEvent).detail;
+    this.files = files;
   }
 
   onProvinceChange() {
